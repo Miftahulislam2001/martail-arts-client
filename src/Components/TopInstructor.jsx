1d4fa63bfb3bb20f70ca30d5a03b0568a5d12dc4
miftahullislam <miftahullislamsohan@gmail.com>
2023-06-17 14:33:13 +0600

    instructor added

--- src/Components/TopInstructor.jsx
+++ src/Components/TopInstructor.jsx
@@ -13,71 +13,77 @@ const TopInstructor = () => {
             <div className='grid grid-cols-1 gap-4 md:grid-cols-3 mt-10'>
 
                 <div className='text-center'>
-                    <img className='object-cover rounded-md h-96 w-96 ' src="https://i.ibb.co/YTjWmmX/licensed-image-2.jpg" alt="" />
+                    <img className='object-cover rounded-md h-96 w-96 shadow-lg' src="/instr/instr1.png" alt="" />
                     <div className='relative bg-[#e85f47] opacity-95 text-white bottom-10 w-[300px] mx-auto rounded-full p-2'>
                         <div className=''>
-                            <h1 className='mt-4 text-2xl font-semibold'>candy sharme</h1>
+                            <h1 className='mt-4 text-2xl font-semibold'> Rafael Lovato Jr.</h1>
                         </div>
                         <div>
-                            <p className='my-2'>candyhad@gmai.com</p>
+                            <p className='my-2'>rafael.@gmai.com</p>
                         </div>
                     </div>
                 </div>
                 <div className='text-center'>
-                    <img className='object-cover rounded-md h-96 w-96 ' src="https://i.ibb.co/YTjWmmX/licensed-image-2.jpg" alt="" />
+                    <img className='object-cover rounded-md h-96 w-96 shadow-lg' src="/public/instr/instr2.jpg" alt="" />
                     <div className='relative bg-[#e85f47] opacity-95 text-white bottom-10 w-[300px] mx-auto rounded-full p-2'>
                         <div className=''>
-                            <h1 className='mt-4 text-2xl font-semibold'>candy sharme</h1>
+                            <h1 className='mt-4 text-2xl font-semibold'>Sifu Liang Tian</h1>
                         </div>
                         <div>
-                            <p className='my-2'>candyhad@gmai.com</p>
+                            <p className='my-2'>sifuian@gmai.com</p>
                         </div>
                     </div>
                 </div>
                 <div className='text-center'>
-                    <img className='object-cover rounded-md h-96 w-96 ' src="https://i.ibb.co/YTjWmmX/licensed-image-2.jpg" alt="" />
+                    <img className='object-cover rounded-md h-96 w-96 shadow-lg' src="/public/instr/instr3.png" alt="" />
                     <div className='relative bg-[#e85f47] opacity-95 text-white bottom-10 w-[300px] mx-auto rounded-full p-2'>
                         <div className=''>
-                            <h1 className='mt-4 text-2xl font-semibold'>candy sharme</h1>
+                            <h1 className='mt-4 text-2xl font-semibold'>Shihan Aiko Tanaka</h1>
                         </div>
                         <div>
-                            <p className='my-2'>candyhad@gmai.com</p>
+                            <p className='my-2'>tanaka@gmai.com</p>
                         </div>
                     </div>
                 </div>
                 <div className='text-center'>
-                    <img className='object-cover rounded-md h-96 w-96 ' src="https://i.ibb.co/YTjWmmX/licensed-image-2.jpg" alt="" />
+                    <img className='object-cover rounded-md h-96 w-96 shadow-lg' src="/public/instr/instr4.jpg" alt="" />
                     <div className='relative bg-[#e85f47] opacity-95 text-white bottom-10 w-[300px] mx-auto rounded-full p-2'>
                         <div className=''>
-                            <h1 className='mt-4 text-2xl font-semibold'>candy sharme</h1>
+                            <h1 className='mt-4 text-2xl font-semibold'>Wong Kiew Kit</h1>
                         </div>
                         <div>
-                            <p className='my-2'>candyhad@gmai.com</p>
+                            <p className='my-2'>wongiewit@gmai.com</p>
                         </div>
                     </div>
                 </div>
                 <div className='text-center'>
-                    <img className='object-cover rounded-md h-96 w-96 ' src="https://i.ibb.co/YTjWmmX/licensed-image-2.jpg" alt="" />
+                    <img className='object-cover rounded-md h-96 w-96 shadow-lg' src="/public/instr/instr5.jpg" alt="" />
                     <div className='relative bg-[#e85f47] opacity-95 text-white bottom-10 w-[300px] mx-auto rounded-full p-2'>
                         <div className=''>
-                            <h1 className='mt-4 text-2xl font-semibold'>candy sharme</h1>
+                            <h1 className='mt-4 text-2xl font-semibold'>Master Chen Zhihua</h1>
                         </div>
                         <div>
-                            <p className='my-2'>candyhad@gmai.com</p>
+                            <p className='my-2'>zhihua@gmai.com</p>
                         </div>
                     </div>
                 </div>
                 <div className='text-center'>
-                    <img className='object-cover rounded-md h-96 w-96 ' src="https://i.ibb.co/YTjWmmX/licensed-image-2.jpg" alt="" />
+                    <img className='object-cover rounded-md h-96 w-96 shadow-lg' src="/public/instr/instr6.jpg" alt="" />
                     <div className='relative bg-[#e85f47] opacity-95 text-white bottom-10 w-[300px] mx-auto rounded-full p-2'>
                         <div className=''>
-                            <h1 className='mt-4 text-2xl font-semibold'>candy sharme</h1>
+                            <h1 className='mt-4 text-2xl font-semibold'>Sensei Hiroshi Nakamura</h1>
                         </div>
                         <div>
-                            <p className='my-2'>candyhad@gmai.com</p>
+                            <p className='my-2'>hiroshiakamura@gmai.com</p>
                         </div>
                     </div>
                 </div>
+
+                
+
+
+
+
 
 
             </div>
